8323b9d9cea7d09422163f6b45c8c000e4fa9487
Miquel A. Cabot <miquel.cabot@gmail.com>
2020-04-06 17:06:40 +0200

    Login to Google with Oauth2

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Inici from '@/views/Inici'
+import * as clientSecret from '../client_secret.json'
+
+const {google} = require('googleapis')
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/admin.directory.user']
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   routes: [
     {
       path: '/',
@@ -36,3 +42,69 @@ export default new Router({
     }
   ]
 })
+
+// Before each route, check for authentication
+router.beforeEach(async (to, from, next) => {
+  /* eslint-disable camelcase */
+  const {client_secret,   client_id} = clientSecret.web
+  const oauth2Client = new google.auth.OAuth2(
+    client_id, client_secret, 'http://localhost:8080')
+
+  // Si retornam del login de Google, capturam el paràmetre 'code'
+  const urlParams = new URLSearchParams(window.location.search)
+  const code = urlParams.get('code')
+
+  let token = localStorage.getItem('token')
+
+  if ((code != null) && (token == null)) {
+    // Ha fet el login amb Google, llegim 'code' retornat
+    oauth2Client.getToken(code, (err, token) => {
+      if (err) return alert('Error retrieving access token' + err)
+      localStorage.setItem('token', JSON.stringify(token))
+      next()
+    })
+  } else if ((code == null) && (token == null)) {
+    // No ha fet login amb Google, hem d'anar a la URL de login
+    const authUrl = oauth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: SCOPES
+    })
+    window.location = authUrl
+  } else {
+    // Ja ha fet login amb Google. Podem continuar
+
+    let token = JSON.parse(localStorage.getItem('token'))
+    oauth2Client.setCredentials(token)
+    listUsers(oauth2Client)
+
+    next()
+  }
+})
+
+/**
+ * Lists the first 10 users in the domain.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+function listUsers (auth) {
+  const service = google.admin({version: 'directory_v1', auth})
+  service.users.list({
+    customer: 'my_customer',
+    maxResults: 10,
+    orderBy: 'email'
+  }, (err, res) => {
+    if (err) return alert('The API returned an error:' + err.message)
+
+    const users = res.data.users
+    if (users.length) {
+      console.log('Users:')
+      users.forEach((user) => {
+        console.log(`${user.primaryEmail} (${user.name.fullName})`)
+      })
+    } else {
+      console.log('No users found.')
+    }
+  })
+}
+
+export default router
