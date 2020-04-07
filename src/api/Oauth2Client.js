08cc7e7567f5a6f9f7dae8d988d89a267c66a843
Miquel A. Cabot <miquel.cabot@gmail.com>
2020-04-07 09:26:40 +0200

    Add scopes

--- src/api/Oauth2Client.js
+++ src/api/Oauth2Client.js
@@ -3,7 +3,11 @@ import * as clientSecret from '../client_secret.json'
 const {google} = require('googleapis')
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/admin.directory.user']
+const SCOPES = [
+  'https://www.googleapis.com/auth/admin.directory.user',
+  'https://www.googleapis.com/auth/admin.directory.group',
+  'https://www.googleapis.com/auth/admin.directory.orgunit',
+  'https://www.googleapis.com/auth/spreadsheets']
 
 /* eslint-disable camelcase */
 const {client_secret, client_id} = clientSecret.web
@@ -32,5 +36,30 @@ let oauth2ClientService = () => {
   const auth = oauth2Client()
   return google.admin({version: 'directory_v1', auth})
 }
+
+/**
+ * Lists the first 10 users in the domain.
+ */
+/*
+function getDomainGroups() {
+  oauth2ClientService().users.list({
+    customer: 'my_customer',
+    maxResults: 10,
+    orderBy: 'email',
+  }, (err, res) => {
+    if (err) return console.error('The API returned an error:', err.message);
+
+    const users = res.data.users;
+    if (users.length) {
+      console.log('Users:');
+      users.forEach((user) => {
+        console.log(`${user.primaryEmail} (${user.name.fullName})`);
+      });
+    } else {
+      console.log('No users found.');
+    }
+  });
+}
+*/
 
 export {oauth2Client, oauth2ClientGenerateAuthUrl, oauth2ClientService}
