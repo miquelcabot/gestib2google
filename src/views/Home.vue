61b8f387bf761c9f26ac52418592669dbe735d3b
Miquel A. Cabot <miquel.cabot@gmail.com>
2020-04-11 12:25:58 +0200

    Show configuration in home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,16 +6,32 @@
     <div class="card-body">
         <p>Benvingut al programa d'importació de dades del GestIB al domini de Google.</p>
         <p>El fitxer XML amb les dades del professorat/alumnat s'ha de descarregar del <a href="https://www3.caib.es/xestib/">GestIB</a>, de l'opció de menú "Alumnat --> Exportació dades SGD".</p>
-        <p>S'actualitzaran les dades del professorat i l'alumnat del domini Google <strong>DOMAIN</strong>.</p>
-        <p>Només s'actualitzaran els usuaris del domini que estiguin a les unitats organitzatives <strong>"/"</strong>, <strong>"TEACHERS_ORGANIZATIONAL_UNIT"</strong> i <strong>"STUDENTS_ORGANIZATIONAL_UNIT"</strong> i tenguin un Employee_ID.</p>
-        <p>Els nous usuaris es crearan amb el password per defecte <strong>"DEFAULT_PASSWORD"</strong> i l'hauran de canviar el primer pic que entrin.</p>
+        <p>S'actualitzaran les dades del professorat i l'alumnat del domini Google <strong>{{ domain }}</strong>.</p>
+        <p>Només s'actualitzaran els usuaris del domini que estiguin a les unitats organitzatives <strong>"/"</strong>, <strong>"{{ organizationalUnitTeachers }}"</strong> i <strong>"{{ organizationalUnitStudents }}"</strong> i tenguin un Employee_ID.</p>
+        <p>Els nous usuaris es crearan amb el password per defecte <strong>"{{ defaultPassword }}"</strong> i l'hauran de canviar el primer pic que entrin.</p>
     </div>
   </div>
 </template>
 
 <script>
+import * as config from '../config.json'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  data () {
+    return {
+      domain: '',
+      organizationalUnitTeachers: '',
+      organizationalUnitStudents: '',
+      defaultPassword: ''
+    }
+  },
+  mounted () {
+    this.domain = config.domain
+    this.organizationalUnitTeachers = config.organizationalUnitTeachers
+    this.organizationalUnitStudents = config.organizationalUnitStudents
+    this.defaultPassword = config.defaultPassword
+  }
 }
 </script>
 
