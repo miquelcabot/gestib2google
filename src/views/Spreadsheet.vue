d8223a3d87c30d216c196555a13104187d89f89c
miquelcabot <miquel.cabot@gmail.com>
2020-12-28 20:02:35 +0100

    Exportacio a full de calcul ordenada (asincrona)

--- src/views/Spreadsheet.vue
+++ src/views/Spreadsheet.vue
@@ -44,95 +44,6 @@ export default {
     }
   },
   methods: {
-  /*  spreadsheet () {
-      // KKK TODO: Arreglar errors exportació a full de càlcul
-      this.loading = true
-      getDomainUsers(null, (err, users) => {
-        if (err) {
-          this.errors.push('Error llegint usuaris "' + err.message + '"')
-          this.loading = false
-        } else {
-          // Array amb informació a exportar al full de càlcul
-          let sheetUsers = []
-          // Per cada usuari del domini...
-          Object.keys(users).forEach(user => {
-            if (!users[user].suspended) {
-              // Grup professorat
-              if (users[user].teacher) {
-                if (!sheetUsers['Professorat']) {
-                  sheetUsers['Professorat'] = []
-                }
-                sheetUsers['Professorat'].push([
-                  users[user].surname + ', ' + users[user].name,
-                  users[user].domainemail]
-                )
-              }
-              // Grups alumnes
-              users[user].groups.forEach(group => {
-                if (group.includes(config.groupPrefixStudents)) {
-                  if (!sheetUsers[group]) {
-                    sheetUsers[group] = []
-                  }
-                  sheetUsers[group].push([
-                    users[user].surname + ', ' + users[user].name,
-                    users[user].domainemail]
-                  )
-                }
-              })
-            }
-          })
-          // Ordenam per grup
-          let sheetUsersOrdered = []
-          Object.keys(sheetUsers).sort().forEach(key => {
-            sheetUsersOrdered[key] = sheetUsers[key]
-          })
-
-          this.filename = 'Usuaris domini ' + (new Date()).toLocaleString()
-          // Cream el full de càlcul a Google Drive
-          // https://developers.google.com/sheets/api/guides/create#nodejs
-          oauth2ClientServiceSheets().spreadsheets.create({
-            resource: {
-              properties: {
-                title: this.filename
-              }
-            },
-            fields: 'spreadsheetId'
-          }, (err, spreadsheet) => {
-            if (err) {
-              this.errors.push('Error creant el full de càlcul "' + err.message + '"')
-              this.loading = false
-            } else {
-              // Si s'ha creat correctament el full de càlcul...
-
-              // Per cada grup...
-              Object.keys(sheetUsersOrdered).forEach((group, indexgroup) => {
-                // Ordenam alfabèticament els membres del grup
-                sheetUsersOrdered[group].sort((a, b) => { return (a[0] < b[0] ? -1 : 1) })
-              })
-              let spreadsheetId = spreadsheet.data.spreadsheetId
-              console.log(sheetUsersOrdered['professorat'])
-              // Afegim valors (membres del grup) al full
-              oauth2ClientServiceSheets().spreadsheets.values.update({
-                spreadsheetId: spreadsheetId,
-                range: 'A1',
-                valueInputOption: 'USER_ENTERED',
-                resource: {
-                  values: sheetUsersOrdered['ifc21a']
-                }
-              }, (err, response) => {
-                if (err) {
-                  this.errors.push('Error afegint les dades al full de càlcul "' + err.message + '"')
-                } else {
-                  this.loading = false
-                  this.$bvModal.show('modal-ok')
-                }
-              })
-            }
-          })
-        }
-      })
-    }
-  } */
     spreadsheet () {
       this.loading = true
       getDomainUsers(null, (err, users) => {
@@ -192,73 +103,85 @@ export default {
             } else {
               // Si s'ha creat correctament el full de càlcul...
               let spreadsheetId = spreadsheet.data.spreadsheetId
-
-              // Per cada grup...
-              Object.keys(sheetUsersOrdered).forEach((group, indexgroup) => {
-                console.log(indexgroup + group)
-
-                // Ordenam alfabèticament els membres del grup
-                sheetUsersOrdered[group].sort((a, b) => { return (a[0] < b[0] ? -1 : 1) })
-                // Afegim capçalera
-                sheetUsersOrdered[group].unshift(['Nom', 'Email'])
-
-                // Afegim un nou full per cada grup
-                oauth2ClientServiceSheets().spreadsheets.batchUpdate({
-                  spreadsheetId: spreadsheetId,
-                  resource: {
-                    requests: {
-                      addSheet: {
-                        properties: {
-                          title: group
-                        }
-                      }
-                    }
-                  }
-                }, (err, response) => {
-                  if (err) {
-                    this.errors.push('Error afegint el grup "' + group + '" al full de càlcul "' + err.message + '"')
-                  } else {
-                    // Afegim valors (membres del grup) al full
-                    oauth2ClientServiceSheets().spreadsheets.values.update({
-                      spreadsheetId: spreadsheetId,
-                      range: group + '!A1',
-                      valueInputOption: 'USER_ENTERED',
-                      resource: {
-                        values: sheetUsersOrdered[group]
-                      }
-                    }, (err, response) => {
-                      if (err) {
-                        this.errors.push('Error afegint les dades del grup "' + group + '" al full de càlcul "' + err.message + '"')
-                      }
-                    })
-                    // Esborram el primer full (sense nom)
-                    console.log((indexgroup + 1) + ' de ' + Object.keys(sheetUsersOrdered).length)
-                    if (indexgroup === (Object.keys(sheetUsersOrdered).length - 1)) {
-                      oauth2ClientServiceSheets().spreadsheets.batchUpdate({
-                        spreadsheetId,
-                        resource: {
-                          requests: {
-                            deleteSheet: {
-                              sheetId: 0
-                            }
-                          }
-                        }
-                      }, (err) => {
-                        if (err) {
-                          this.errors.push('Error esborrant les pàgines buides del full de càlcul "' + err.message + '"')
-                        } else {
-                          this.loading = false
-                          this.$bvModal.show('modal-ok')
-                        }
-                      })
-                    }
-                  }
-                })
+              this.addSheet(spreadsheetId, sheetUsersOrdered, 0, () => {
+                this.loading = false
+                this.$bvModal.show('modal-ok')
               })
             }
           })
         }
       })
+    },
+    addSheet (spreadsheetId, sheetUsersOrdered, index, callback) {
+      let groups = Object.keys(sheetUsersOrdered)
+      if (index >= groups.length) {
+        // Hem arribat al final de l'array. Tornar
+        callback()
+      } else {
+        // Per cada grup...
+        let group = groups[index]
+
+        // Ordenam alfabèticament els membres del grup
+        sheetUsersOrdered[group].sort((a, b) => a.toString().localeCompare(b))
+        // Afegim capçalera
+        sheetUsersOrdered[group].unshift(['Nom', 'Email'])
+
+        // Afegim un nou full per cada grup
+        oauth2ClientServiceSheets().spreadsheets.batchUpdate({
+          spreadsheetId: spreadsheetId,
+          resource: {
+            requests: {
+              addSheet: {
+                properties: {
+                  title: group
+                }
+              }
+            }
+          }
+        }, (err, response) => {
+          if (err) {
+            this.errors.push('Error afegint el grup "' + group + '" al full de càlcul "' + err.message + '"')
+            this.loading = false
+          } else {
+            // En acabar d'afegir el primer grup, eliminar el primer full
+            if (index === 0) {
+              oauth2ClientServiceSheets().spreadsheets.batchUpdate({
+                spreadsheetId,
+                resource: {
+                  requests: {
+                    deleteSheet: {
+                      sheetId: 0
+                    }
+                  }
+                }
+              }, (err) => {
+                if (err) {
+                  this.errors.push('Error esborrant les pàgines buides del full de càlcul "' + err.message + '"')
+                  this.loading = false
+                }
+              })
+            }
+
+            // Afegim valors (membres del grup) al full
+            oauth2ClientServiceSheets().spreadsheets.values.update({
+              spreadsheetId: spreadsheetId,
+              range: group + '!A1',
+              valueInputOption: 'USER_ENTERED',
+              resource: {
+                values: sheetUsersOrdered[group]
+              }
+            }, (err, response) => {
+              if (err) {
+                this.errors.push('Error afegint les dades del grup "' + group + '" al full de càlcul "' + err.message + '"')
+                this.loading = false
+              } else {
+                // Cridam recursivament el següent grup
+                this.addSheet(spreadsheetId, sheetUsersOrdered, index + 1, callback)
+              }
+            })
+          }
+        })
+      }
     }
   }
 }
