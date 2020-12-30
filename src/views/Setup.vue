f675dab0aee8990516b63423d857de161ae66211
miquelcabot <miquel.cabot@gmail.com>
2020-12-30 23:51:16 +0100

    Afegim paràmetres de confirguració del email

--- src/views/Setup.vue
+++ src/views/Setup.vue
@@ -44,6 +44,48 @@
             <option v-bind:value="true">Format llarg (apalou@domini)</option>
           </select>
         </div>
+        <hr class="separator">
+        <div class="alert alert-warning" role="alert">
+          Per especificar el format dels emails d'alumnat i professorat es pot especificar:
+          <ul>
+            <li>Prefix, abans del nom d'usuari</li>
+            <li>Format del nom d'usuari
+              <ul>
+                <li>"n" inicial del nom, o "nnn" nom complet</li>
+                <li>"1" inicial del 1r llinatge, o "111" 1r llinatge complet</li>
+                <li>"2" inicial del 2n llinatge, o "222" 2n llinatge complet</li>
+                <li>"0" nombres aleatoris</li>
+                <li>",.-" altres caràcters</li>
+              </ul>
+            </li>
+            <li>Domini personalitzat</li>
+          </ul>
+        </div>
+        <div class="form-group">
+          <label for="teachersEmailPrefix">Prefix de l'email del professorat</label>
+          <input class="form-control" id="teachersEmailPrefix" name="teachersEmailPrefix" type="text" v-model="teachersEmailPrefix" disabled>
+        </div>
+        <div class="form-group">
+          <label for="teachersEmailFormat">Format de l'email del professorat</label>
+          <input class="form-control" id="teachersEmailFormat" name="teachersEmailFormat" type="text" v-model="teachersEmailFormat" disabled>
+        </div>
+        <div class="form-group">
+          <label for="teachersEmailCustomDomain">Domini personalitzat de l'email del professorat</label>
+          <input class="form-control" id="teachersEmailCustomDomain" name="teachersEmailCustomDomain" type="text" v-model="teachersEmailCustomDomain" disabled>
+        </div>
+        <hr class="separator">
+        <div class="form-group">
+          <label for="studentsEmailPrefix">Prefix de l'email de l'alumnat</label>
+          <input class="form-control" id="studentsEmailPrefix" name="studentsEmailPrefix" type="text" v-model="studentsEmailPrefix" disabled>
+        </div>
+        <div class="form-group">
+          <label for="studentsEmailFormat">Format de l'email de l'alumnat</label>
+          <input class="form-control" id="studentsEmailFormat" name="studentsEmailFormat" type="text" v-model="studentsEmailFormat" disabled>
+        </div>
+        <div class="form-group">
+          <label for="studentsEmailCustomDomain">Domini personalitzat de l'email de l'alumnat</label>
+          <input class="form-control" id="studentsEmailCustomDomain" name="studentsEmailCustomDomain" type="text" v-model="studentsEmailCustomDomain" disabled>
+        </div>
         <!--
         <div class="form-group">
           <button class="btn btn-secondary" v-on:click="saveConfig()" disabled>Restablir</button>
@@ -70,7 +112,13 @@ export default {
       groupPrefixTutors: '',
       organizationalUnitTeachers: '',
       organizationalUnitStudents: '',
-      longStudentsEmail: false
+      longStudentsEmail: false,
+      teachersEmailPrefix: '',
+      teachersEmailFormat: '',
+      teachersEmailCustomDomain: '',
+      studentsEmailPrefix: '',
+      studentsEmailFormat: '',
+      studentsEmailCustomDomain: ''
     }
   },
   mounted () {
@@ -83,6 +131,12 @@ export default {
     this.organizationalUnitTeachers = config().organizationalUnitTeachers
     this.organizationalUnitStudents = config().organizationalUnitStudents
     this.longStudentsEmail = config().longStudentsEmail
+    this.teachersEmailPrefix = config().teachersEmailPrefix
+    this.teachersEmailFormat = config().teachersEmailFormat
+    this.teachersEmailCustomDomain = config().teachersCustomDomain
+    this.studentsEmailPrefix = config().studentsEmailPrefix
+    this.studentsEmailFormat = config().studentsEmailFormat
+    this.studentsEmailCustomDomain = config().studentsCustomDomain
   },
   methods: {
     saveConfig () {
